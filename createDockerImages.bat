06f201ae351cea4bdd669cbb950c4c681bf97f87
fin.kingma <fin.kingma@ns.nl>
2025-06-17 23:11:34 +0200

    dockerfile now also ready for angular frontend

--- createDockerImages.bat
+++ createDockerImages.bat
@@ -9,3 +9,5 @@ CALL mvn clean compile jib:build
 cd ..
 CALL docker build -f blinddatingapp-frontend/Dockerfile -t docker.io/finkingma/blinddatingapp-frontend:latest .
 CALL docker push docker.io/finkingma/blinddatingapp-frontend:latest
+CALL docker build -f blinddatingapp-frontend-angular/Dockerfile -t docker.io/finkingma/blinddatingapp-frontend-angular:latest .
+CALL docker push docker.io/finkingma/blinddatingapp-frontend-angular:latest
