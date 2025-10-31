3c640a18be77ca641f8dc5db48c77ce395eeefc3
fin.kingma <fin.kingma@ns.nl>
2025-10-31 13:37:02 +0100

    added profile service logging

--- profile-service/src/main/java/io/praegus/bda/profileservice/adapter/location/LocationServiceAdapter.java
+++ profile-service/src/main/java/io/praegus/bda/profileservice/adapter/location/LocationServiceAdapter.java
@@ -3,12 +3,16 @@ package io.praegus.bda.profileservice.adapter.location;
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.api.LocationsApi;
 import org.openapitools.client.model.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpClientErrorException;
 
 @Repository
 public class LocationServiceAdapter {
+
+    Logger logger = LoggerFactory.getLogger(LocationServiceAdapter.class);
 
     private final LocationsApi locationsApi;
 
@@ -23,6 +27,7 @@ public class LocationServiceAdapter {
         try {
             return locationsApi.retrieveLocation(address);
         } catch (HttpClientErrorException.NotFound exception) {
+            logger.error("Error when retrieving location", exception);
             address.valid(false);
             return address;
         }
