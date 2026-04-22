199f338a9b77564514da520bb935f97cd67ddbc0
finkingma <finkingma@gmail.com>
2026-04-22 20:46:07 +0200

    feat(springboot/java upgrade): upgraded springboot 4
    Refs #94

--- profile-service/src/test/java/io/praegus/bda/profileservice/SupportsPostgresIntegrationTests.java
+++ profile-service/src/test/java/io/praegus/bda/profileservice/SupportsPostgresIntegrationTests.java
@@ -2,7 +2,7 @@ package io.praegus.bda.profileservice;
 
 import io.praegus.bda.profileservice.support.LocationServiceLandscapeInitializer;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
